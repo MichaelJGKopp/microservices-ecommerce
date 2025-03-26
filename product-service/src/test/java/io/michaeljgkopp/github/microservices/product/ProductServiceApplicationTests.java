e33f5e04d1385140bedf9126f2de97cf4264e118
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-26 19:02:34 +0100

    test post method with RestAssured amd MongoDBContainer

--- product-service/src/test/java/io/michaeljgkopp/github/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/io/michaeljgkopp/github/microservices/product/ProductServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package io.michaeljgkopp.github.microservices.product;
 
+import io.restassured.RestAssured;
+import lombok.RequiredArgsConstructor;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// embedded web server will start on a random port, avoiding port conflicts during tests
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @ServiceConnection  // spring boot will automatically configure the connection to the container
+    static MongoDBContainer mongoDBContainer =
+            new MongoDBContainer("mongo:7.0.5");
 
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() {
+        String requestBody = """
+                {
+                	"name": "iPhone 15",
+                    "description": "iPhone 15 is a smartphone from Apple",
+                    "price": 1000
+                }
+                """;
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/product")
+                .then()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo("iPhone 15"))
+                .body("description", Matchers.equalTo("iPhone 15 is a smartphone from Apple"))
+                .body("price", Matchers.equalTo(1000));
+    }
 }
