6d713fd7fb9fe31e5c7e31d911e4e032fb5ad0d9
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-26 20:58:58 +0100

    add order-service 2nd microservice

---
+++ order-service/src/test/java/io/michaeljgkopp/github/microservices/order/TestOrderServiceApplication.java
@@ -0,0 +1,11 @@
+package io.michaeljgkopp.github.microservices.order;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
