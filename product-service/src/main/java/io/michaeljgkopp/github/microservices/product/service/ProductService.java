32c9a6507ca2c1664dd524a2512902919539f063
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-26 11:23:27 +0100

    add ProductController and list all products

--- product-service/src/main/java/io/michaeljgkopp/github/microservices/product/service/ProductService.java
+++ product-service/src/main/java/io/michaeljgkopp/github/microservices/product/service/ProductService.java
@@ -3,6 +3,10 @@ package io.michaeljgkopp.github.microservices.product.service;
 import io.michaeljgkopp.github.microservices.product.dto.ProductRequest;
 import io.michaeljgkopp.github.microservices.product.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
     Product createProduct(ProductRequest productRequest);
+
+    List<Product> getAllProducts();
 }
--- product-service/src/main/java/io/michaeljgkopp/github/microservices/product/service/ProductServiceImpl.java
+++ product-service/src/main/java/io/michaeljgkopp/github/microservices/product/service/ProductServiceImpl.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
@@ -27,5 +29,10 @@ public class ProductServiceImpl implements ProductService {
         log.info("Product created successfully: {}", product);
 
         return product;
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
     }
 }
