43ce50309fcd17b15e6efac43f03830ed66b409f
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-03-27 21:41:48 +0100

    add log msg to isInStock

--- inventory-service/src/main/java/io/michaeljgkopp/github/microservices/inventory/service/InventoryServiceImpl.java
+++ inventory-service/src/main/java/io/michaeljgkopp/github/microservices/inventory/service/InventoryServiceImpl.java
@@ -2,15 +2,19 @@ package io.michaeljgkopp.github.microservices.inventory.service;
 
 import io.michaeljgkopp.github.microservices.inventory.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryServiceImpl implements InventoryService {
     private final InventoryRepository inventoryRepository;
 
     @Override
     public boolean isInStock(String skuCode, Integer quantity) {
-        return inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+        boolean isInStock = inventoryRepository.existsBySkuCodeAndQuantityIsGreaterThanEqual(skuCode, quantity);
+        log.info("Product with skuCode {} still has {} or more left in stock: {}", skuCode, quantity, isInStock);
+        return isInStock;
     }
 }
