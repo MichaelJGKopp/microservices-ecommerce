979ba5148bee2fa889afc0dff5e1bd72be6a57f4
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-04-11 23:17:19 +0200

    let notification-service listen to kafka topic order-placed

---
+++ notification-service/src/main/java/io/michaeljgkopp/github/microservices/notification/service/NotificationService.java
@@ -0,0 +1,21 @@
+package io.michaeljgkopp.github.microservices.notification.service;
+
+import io.michaeljgkopp.github.microservices.notification.order.OrderPlacedEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationService {
+
+    @KafkaListener(topics = "order-placed")
+    public void listen(OrderPlacedEvent orderPlacedEvent) {
+        log.info("Received message from order-placed topic: {}", orderPlacedEvent);
+        // Send email to customer
+
+
+    }
+}
