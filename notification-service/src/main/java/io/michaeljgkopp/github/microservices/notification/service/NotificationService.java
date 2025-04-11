04e979f6e7da84b1a57679f53908f6ee81614209
MichaelJG.Kopp <michaeljg.kopp@gmail.com>
2025-04-12 00:12:48 +0200

    set up JavaMail; incl. json (web starter) for kafka

--- notification-service/src/main/java/io/michaeljgkopp/github/microservices/notification/service/NotificationService.java
+++ notification-service/src/main/java/io/michaeljgkopp/github/microservices/notification/service/NotificationService.java
@@ -3,7 +3,12 @@ package io.michaeljgkopp.github.microservices.notification.service;
 import io.michaeljgkopp.github.microservices.notification.order.OrderPlacedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,11 +16,40 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class NotificationService {
 
+    @Value("${email-address}")
+    private String EMAIL_ADDRESS;
+    private final JavaMailSender javaMailSender;
+
     @KafkaListener(topics = "order-placed")
     public void listen(OrderPlacedEvent orderPlacedEvent) {
         log.info("Received message from order-placed topic: {}", orderPlacedEvent);
+
         // Send email to customer
-
-
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom(EMAIL_ADDRESS);
+            messageHelper.setTo(orderPlacedEvent.getEmail());
+            messageHelper.setSubject(String.format("Your Order with OrderNumber %s is placed successfully", orderPlacedEvent.getOrderNumber()));
+            messageHelper.setText(String.format("""
+                            Hi %s,%s
+                            
+                            Your order with order number %s is now placed successfully.
+                            
+                            Best Regards
+                            Spring Shop
+                            """,
+                    "firstName",    // TODO: replace with orderPlacedEvent.getFirstName().toString()
+                    "lastName",
+//                    orderPlacedEvent.getFirstName().toString(),
+//                    orderPlacedEvent.getLastName().toString(),
+                    orderPlacedEvent.getOrderNumber()));
+        };
+        try {
+            javaMailSender.send(messagePreparator);
+            log.info("Order notification email sent to customer.");
+        } catch (MailException e) {
+            log.error("Exception occurred when sending mail", e);
+            throw new RuntimeException("Exception occurred when sending mail to " + EMAIL_ADDRESS, e);
+        }
     }
 }
